dd41950ed257aad2e36eede177b214e6b97105e4
H.Ekko <eric_huangyz@163.com>
2017-07-03 23:15:44 +0800

    Add files via upload

---
+++ todo/todo/Classes/Base/Controller/TDNavigationController.swift
@@ -0,0 +1,41 @@
+//
+//  TDNavigationController.swift
+//  todo
+//
+//  Created by HuangZhen on 2017/7/3.
+//  Copyright © 2017年 Ekko. All rights reserved.
+//
+
+import UIKit
+
+class TDNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+        if childViewControllers.count > 0 {
+            viewController.hidesBottomBarWhenPushed = true
+        }
+        super.pushViewController(viewController, animated: animated)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
