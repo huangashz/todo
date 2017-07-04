b3009935370d09682026e873d3e038af34cc17bf
HuangZhen <zhenhuang@sohu-inc.com>
2017-07-04 19:15:59 +0800

    添加basetabbar

---
+++ todo/todo/Classes/Base/Controller/TDTabBarController.swift
@@ -0,0 +1,50 @@
+//
+//  TDTabBarController.swift
+//  todo
+//
+//  Created by HuangZhen on 04/07/2017.
+//  Copyright © 2017 Ekko. All rights reserved.
+//
+
+import UIKit
+
+class TDTabBarController: UITabBarController {
+
+    override func loadView() {
+        super.loadView()
+        self.tabBar.barTintColor = UIColor.white
+        let item :UITabBarItem = UITabBarItem.appearance()
+        let selectedAttrs : [String : Any] = [NSForegroundColorAttributeName : UIColor.black]
+        let normalAttrs : [String : Any] = [NSFontAttributeName : UIFont.systemFont(ofSize: 12)]
+        item.setTitleTextAttributes(selectedAttrs, for: .selected)
+        item.setTitleTextAttributes(normalAttrs, for: .normal)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupAllChildVC()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func setupAllChildVC() {
+        let mainVC : TDNavigationController = TDNavigationController.init(rootViewController:TDBaseViewController())
+        mainVC.tabBarItem.title = "Ashe"
+         self.viewControllers = [mainVC]
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ todo/todo/Classes/Base/Controller/TDTaskListViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TDTaskListViewController.swift
+//  todo
+//
+//  Created by HuangZhen on 04/07/2017.
+//  Copyright © 2017 Ekko. All rights reserved.
+//
+
+import UIKit
+
+class TDTaskListViewController: TDBaseTableViewController {
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
